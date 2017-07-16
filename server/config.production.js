9ce152aa1c2f5794c289f33d85bcd754b2ec6169
Ankit <ankit.sari@gmail.com>
2017-07-16 16:21:20 +0530

    init commit

---
+++ server/config.production.js
@@ -0,0 +1,30 @@
+module.exports = {
+  host: process.env.HOST,
+  port: process.env.PORT,
+  UIHOST: process.env.UIHOST,
+  UIPROTOCOL: process.env.UIPROTOCOL,
+  UIPORT: process.env.UIPORT,
+  EMAILFROM: process.env.EMAILFROM,
+  UIURL: process.env.UIURL,
+  APIKEY: process.env.APIKEY,
+  cloudStoreImages: {
+    s3: {
+      bucket: process.env.AWS_S3_BUCKET ,
+      region: process.env.AWS_S3_REGION ,
+      accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID ,
+      secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY
+    },
+    cacheControl:1296000,
+  },
+  VALID_IMAGE_EXT: ['jpg', 'jpeg', 'png', 'gif'],
+  cloudStoreVideos: {
+    s3: {
+      bucket: process.env.AWS_S3_BUCKET ,
+      region: process.env.AWS_S3_REGION ,
+      accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID ,
+      secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY
+    },
+    cacheControl:1296000
+  },
+  VALID_VIDEO_EXT: ['mp4']
+};
---
+++ server/datasources.production.js
@@ -0,0 +1,37 @@
+module.exports = {
+  mongoDb: {
+    "name": "mongoDb",
+    "connector": "loopback-connector-mongodb",
+    "url": process.env.DB_URL + process.env.DB_NAME
+  },
+  "sparkpost": {
+    "connector": "loopback-connector-sparkpost",
+    "apiKey": process.env.APIKEY,
+    "defaults": {
+      "options": {
+        "start_time": "now",
+        "from": process.env.EMAILFROM,
+        "sandbox": true
+      }
+    }
+  },
+  fileStorageDS: {
+    root: "./upload",
+    acl: 'public-read',
+    maxFileSize: process.env.MAX_IMAGE_UPLOAD_SIZE * 1024 *1024,
+    getFilename: function(fileInfo) {
+      var fileName = fileInfo.name.replace(/\s+/g, '-').toLowerCase();
+      return 'image-' + new Date().getTime() + '-' + fileName;
+    }
+  },
+  fileStorageVideo: {
+    root: "./upload",
+    acl: 'public-read',
+
+    maxFileSize: process.env.MAX_VIDEO_UPLOAD_SIZE * 1024 *1024,
+    getFilename: function (fileInfo) {
+      var fileName = fileInfo.name.replace(/\s+/g, '-').toLowerCase();
+      return 'video-' + new Date().getTime() + '-' + fileName;
+    }
+  }
+};
